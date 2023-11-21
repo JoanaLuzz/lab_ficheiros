0a58442e732e89369cf271969e2a53adc19f4f48
Joana Luz <joana.luz.remelhe@tecnico.ulisboa.pt>
2023-11-21 14:23:08 +0000

    mudanças

--- open-read/open-read.c
+++ open-read/open-read.c
@@ -5,52 +5,44 @@
  *
  */
 
-#include <stdio.h>
 #include <errno.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[]) {
+    /*
+     *
+     * the attributes are:
+     * - O_RDONLY: open the file for reading
+     *
+     */
+    // FILE *fopen(const char *pathname, const char *mode);
+    FILE *fd = fopen("test.txt", "r");
+    if (fd < 0) {
+        fprintf(stderr, "open error: %s\n", strerror(errno));
+        return -1;
+    }
 
-int main(int argc, char *argv[])
-{
-   /*
-    *
-    * the attributes are:
-    * - O_RDONLY: open the file for reading
-    *
-    */
-   int fd = open("test.txt", O_RDONLY);
-   if (fd < 0){
-      fprintf(stderr, "open error: %s\n", strerror(errno));
-      return -1;
-   }
+    char buffer[128];
+    memset(buffer, 0, sizeof(buffer));
 
-   char buffer[128];
-   memset(buffer,0,sizeof(buffer));
+    /* read the contents of the file */
+    while (!feof(fd)) {
+        int bytes_read = fread(buffer, 1, sizeof(buffer) - 1, fd);
+        if (bytes_read == 0 && ferror(fd)) {
+            fprintf(stderr, "read error: %s\n", strerror(errno));
+            return -1;
+        }
+        buffer[bytes_read] = '\0';
+        printf("%s", buffer);
+    }
 
-   /* read the contents of the file */
-   int done = 0;
-   while (done < sizeof(buffer) - 1){
-      int bytes_read = read(fd, buffer + done, sizeof(buffer) - 1 - done);
-      if (bytes_read < 0){
-         fprintf(stderr, "read error: %s\n", strerror(errno));
-         return -1;
-      }
+    /* close the file */
+    fclose(fd);
 
-      /* if we read 0 bytes, we're done */
-      if (bytes_read == 0)
-         break;
-
-      done += bytes_read;
-   }
-
-   printf("%s",buffer);
-
-   /* close the file */
-   close(fd);
-
-   return 0;
+    return 0;
 }
--- open-write/open-write.c
+++ open-write/open-write.c
@@ -5,52 +5,51 @@
  *
  */
 
-#include <stdio.h>
 #include <errno.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-   /*
-    * the attributes are:
-    * - O_CREAT: create the file if it does not exist
-    * - O_TRUNC: erase the contents of the file
-    * - O_WRONLY: open the file for writing
-    *
-    * the permissions are applied if the file is created:
-    * - S_IRUSR: reading, user
-    * - S_IWUSR: writing, user
-    */
-   int fd = open("test.txt", O_CREAT | O_TRUNC | O_WRONLY , S_IRUSR | S_IWUSR);
-   if (fd < 0){
-      fprintf(stderr, "open error: %s\n", strerror(errno));
-      return -1;
-   }
+int main(int argc, char *argv[]) {
+    /*
+     * the attributes are:
+     * - O_CREAT: create the file if it does not exist
+     * - O_TRUNC: erase the contents of the file
+     * - O_WRONLY: open the file for writing
+     *
+     * the permissions are applied if the file is created:
+     * - S_IRUSR: reading, user
+     * - S_IWUSR: writing, user
+     */
 
-   char buffer[] = "Hello World!\n";
+    FILE *fd = fopen("test.txt", "w");
+    if (fd < 0) {
+        fprintf(stderr, "open error: %s\n", strerror(errno));
+        return -1;
+    }
 
-   /* write a string to the file */
-   int len = strlen(buffer);
-   int done = 0;
-   while (len > 0) {
-      int bytes_written = write(fd, buffer + done, len);
+    char buffer[] = "Hello World!\n";
 
-      if (bytes_written < 0){
-         fprintf(stderr, "write error: %s\n", strerror(errno));
-         return -1;
-      }
+    /* write a string to the file */
+    int len = strlen(buffer);
+    int done = 0;
+    while (len > 0) {
+        int bytes_written = fwrite(buffer + done, 1, len, fd);
+        if (bytes_written < 0) {
+            fprintf(stderr, "write error: %s\n", strerror(errno));
+            return -1;
+        }
 
-      /* might not have managed to write all, len becomes what remains */
-      len -= bytes_written;
-      done += bytes_written;
-   }
+        /* might not have managed to write all, len becomes what remains */
+        len -= bytes_written;
+        done += bytes_written;
+    }
 
-   /* close the file */
-   close(fd);
+    /* close the file */
+    fclose(fd);
 
-   return 0;
+    return 0;
 }
